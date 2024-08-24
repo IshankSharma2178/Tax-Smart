6e74c9539af185c551dc383ee450b09e3a6e7a3f
IshankSharma2178 <ishank.sharma_cs23@gla.ac.in>
2024-08-24 22:49:17 +0530

    SignUpPage

--- src/component/auth/SignupForm.js
+++ src/component/auth/SignupForm.js
@@ -1,131 +1,142 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { NavLink } from 'react-router-dom';
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 function SignupForm() {
 
-  const{register,getValues,setValue , handleSubmit ,reset , formState:{errors , isSubmitSuccessful} } =useForm(); 
+  const { register, getValues, setValue, handleSubmit, reset, formState: { errors, isSubmitSuccessful } } = useForm(); 
+  const [passwordVisibility, setPasswordVisibility] = useState({ password: true, confirmPassword: true });
 
-  useEffect(()=>{
-    if(isSubmitSuccessful){
-        reset({
-            emialAddress:"",firstname:"",lastname:"",confirmPassword:"",password:"",
-        })
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({
+        emailAddress: "", firstname: "", lastname: "", confirmPassword: "", password: "",
+      });
     }
-},[reset,isSubmitSuccessful])
+  }, [reset, isSubmitSuccessful]);
 
-  const submitHandler = (values) =>{
+  const togglePasswordVisibility = (field) => {
+    setPasswordVisibility(prev => ({
+      ...prev,
+      [field]: !prev[field]
+    }));
+  };
+
+  const submitHandler = (values) => {
     console.log('submitHandler');
     console.log(values);
-
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(submitHandler)}>
-        <div className='flex flex-row  w-[100%] gap-4'>
-        <div  className='flex flex-col w-[50%]' >
-                    <label htmlFor='firstname' className='text-richblack-100 pb-2'>First Name <sup className='text-pink-200'>*</sup></label>
-                        <input 
-                            type='text'
-                            name='firstname'
-                            id='firstname'
-                            placeholder='Enter first name'
-                            className="w-full rounded-[0.5rem] outline-none shadow-custom bg-richblack-800 focus:shadow-none  p-[12px] text-richblack-5"
-                            {...register("firstname",{required:true})}
-                        />
-                        {
-                            errors.firstname && (
-                                <span className='text-pink-200 text-sm'>*Please Enter First Name </span>
-                            )
-                        }
+    <div className='flex flex-col gap-6'>
+      <div className='text-[#456297] text-xl'>
+        Sign Up Now, and start your journey on taxation and financial management.
+      </div>
+      <form onSubmit={handleSubmit(submitHandler)} className='flex flex-col gap-4'>
+        <div className='flex flex-row w-[100%] gap-4'>
+          <div className='flex flex-col w-[50%]'>
+            <label htmlFor='firstname' className='text-[#456297] pb-2'>First Name <sup className='text-pink-200'>*</sup></label>
+            <input 
+              type='text'
+              name='firstname'
+              id='firstname'
+              placeholder='Enter first name'
+              className="w-full rounded-[0.5rem] outline-none shadow-custom bg-richblack-800 focus:shadow-none p-[12px] text-richblack-5"
+              {...register("firstname", { required: true })}
+            />
+            {errors.firstname && (
+              <span className='text-pink-200 text-sm'>*Please Enter First Name</span>
+            )}
           </div>
           <div className='flex flex-col w-[50%]'>
-            <label htmlFor='lastname' className='text-richblack-100  pb-2'>Last Name <sup className='text-pink-200'>*</sup></label>
-              <input 
-                type='text'
-                name='lastname'
-                id='lastname'
-                placeholder='Enter last name'
-                className="w-full rounded-[0.5rem] outline-none shadow-custom bg-richblack-800 p-[12px] focus:shadow-none text-richblack-5"
-                {...register("lastname",{required:true})}
-              />
-              {
-                errors.lastname && (
-                    <span className='text-pink-200 text-sm'>Please Enter Last Name </span>
-                )
-              }
+            <label htmlFor='lastname' className='text-[#456297] pb-2'>Last Name <sup className='text-pink-200'>*</sup></label>
+            <input 
+              type='text'
+              name='lastname'
+              id='lastname'
+              placeholder='Enter last name'
+              className="w-full rounded-[0.5rem] outline-none shadow-custom bg-richblack-800 p-[12px] focus:shadow-none text-richblack-5"
+              {...register("lastname", { required: true })}
+            />
+            {errors.lastname && (
+              <span className='text-pink-200 text-sm'>Please Enter Last Name</span>
+            )}
           </div>
         </div>
-        <div  className='flex flex-col w-[100%]' >
-          <label htmlFor='emialAddress' className='text-richblack-100 pb-2'>Email Address <sup className='text-pink-200'>*</sup></label>
-              <input 
-                  type='email'
-                  name='emialAddress'
-                  id='emialAddress'
-                  placeholder='Enter Email Address'
-                  className="w-full rounded-[0.5rem] outline-none shadow-custom bg-richblack-800 focus:shadow-none  p-[12px] text-richblack-5"
-                  {...register("emialAddress",{required:true})}
-              />
-              {
-                  errors.emialAddress && (
-                      <span className='text-pink-200 text-sm'>*Please Email Address </span>
-                  )
-              }
-          </div>
+        <div className='flex flex-col w-[100%]'>
+          <label htmlFor='emailAddress' className='text-[#456297] pb-2'>Email Address <sup className='text-pink-200'>*</sup></label>
+          <input 
+            type='email'
+            name='emailAddress'
+            id='emailAddress'
+            placeholder='Enter Email Address'
+            className="w-full rounded-[0.5rem] outline-none shadow-custom bg-richblack-800 focus:shadow-none p-[12px] text-richblack-5"
+            {...register("emailAddress", { required: true })}
+          />
+          {errors.emailAddress && (
+            <span className='text-pink-200 text-sm'>*Please Enter Email Address</span>
+          )}
+        </div>
 
-          <div className='flex flex-row  w-[100%] gap-4'>
-        <div  className='flex flex-col w-[50%]' >
-                    <label htmlFor='password' className='text-richblack-100 pb-2'>Create Password <sup className='text-pink-200'>*</sup></label>
-                        <input 
-                            type='password'
-                            name='password'
-                            id='password'
-                            placeholder='Enter Password'
-                            className="w-full rounded-[0.5rem] outline-none shadow-custom bg-richblack-800 focus:shadow-none  p-[12px] text-richblack-5"
-                            {...register("password",{required:true})}
-                        />
-                        {
-                            errors.password && (
-                                <span className='text-pink-200 text-sm'>*Please Enter Password </span>
-                            )
-                        }
+        <div className='flex flex-row w-[100%] gap-4'>
+          <div className='flex flex-col w-[50%] relative'>
+            <label htmlFor='password' className='text-[#456297] inp pb-2'>Create Password <sup className='text-pink-200'>*</sup></label>
+            <input 
+              type={passwordVisibility.password ? 'password' : 'text'}
+              name='password'
+              id='password'
+              placeholder='Enter Password'
+              className="w-full rounded-[0.5rem] inp pr-10 outline-none shadow-custom bg-richblack-800 focus:shadow-none p-[12px] text-richblack-5"
+              {...register("password", { required: true })}
+            />
+            
+
+            <div className='absolute text-richblack-25 right-2  cursor-pointer bottom-3  text-2xl'
+              onClick={() => togglePasswordVisibility("password")}>
+              {passwordVisibility.password ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+            </div>
+            {errors.password && (
+              <span className='text-pink-200 text-sm'>*Please Enter Password</span>
+            )}
           </div>
-          <div className='flex flex-col w-[50%]'>
-            <label htmlFor='confirmPassword' className='text-richblack-100  pb-2'>Confirm Password <sup className='text-pink-200'>*</sup></label>
-              <input 
-                type='text'
-                name='confirmPassword'
-                id='confirmPassword'
-                placeholder='Confirm Password'
-                className="w-full rounded-[0.5rem] outline-none shadow-custom bg-richblack-800 p-[12px] focus:shadow-none text-richblack-5"
-                {...register("confirmPassword",{required:true})}
-              />
-              {
-                errors.confirmPassword && (
-                    <span className='text-pink-200 text-sm'>Please Confirm Password </span>
-                )
-              }
+          <div className='flex flex-col w-[50%] relative'>
+            <label htmlFor='confirmPassword' className='text-[#456297] pb-2'>Confirm Password <sup className='text-pink-200'>*</sup></label>
+            <input 
+              type={passwordVisibility.confirmPassword ? 'password' : 'text'}
+              name='confirmPassword'
+              id='confirmPassword'
+              placeholder='Confirm Password'
+              className="w-full rounded-[0.5rem] pr-10 outline-none shadow-custom bg-richblack-800 p-[12px] focus:shadow-none text-richblack-5"
+              {...register("confirmPassword", { required: true })}
+            />
+            <div className='absolute text-richblack-25 right-2 cursor-pointer bottom-3 text-2xl'
+              onClick={() => togglePasswordVisibility("confirmPassword")}>
+              {passwordVisibility.confirmPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+            </div>
+            {errors.confirmPassword && (
+              <span className='text-pink-200 text-sm'>Please Confirm Password</span>
+            )}
           </div>
         </div>
-        
+
         <div className='flex flex-row gap-1'>
-          <p className='text-white'>Already have an account?</p> 
+          <p className='text-[#2d4064]'>Already have an account?</p> 
           <NavLink to={"/login"} className="text-blue-500 underline">
-              Login
+            Login
           </NavLink>
         </div>
-        
+
         <button
           type="submit"
-          className={`text-center text-[20px] font-medium  text-nowrap px-5 py-2 rounded-md bg-yellow-50 w-full text-richblack-900 
-                        shadow-[2px_2px_0px_0px_rgba(255,255,255,0.18)] hover:shadow-none hover:scale-95 transition-all duration-200 `}
+          className={`text-center text-[20px] font-medium text-nowrap px-5 py-2 rounded-md bg-yellow-50 w-full text-richblack-900 
+                        shadow-[2px_2px_0px_0px_rgba(255,255,255,0.18)] hover:shadow-none hover:scale-95 transition-all duration-200`}
         >
           Create Account
         </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default SignupForm
+export default SignupForm;
--- src/component/auth/Template.js
+++ src/component/auth/Template.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import SignupForm from "./SignupForm"
 import LoginForm from './LoginForm'
 import StaticNavbar from '../common/StaticNavbar'
+import LoginImg from "../../assets/img/LoginImg.png"
 
 function Template({img1, img2,heading,subheading1,subheading2,formType}) {
 
@@ -9,16 +10,36 @@ function Template({img1, img2,heading,subheading1,subheading2,formType}) {
 
   return (
     <>
-    <div className='max-w-[1360px] w-[95%] m-auto relative z-[11]'>
+    <div className='max-w-[1360px] w-[95%] m-auto relative z-[11] overflow-hidden'>
         <StaticNavbar margin={"8"} height={"10vh"} />
       </div>
-    <div className="w-screen  bg-richblack-400 m-auto">
-      
-      {loading ? (
+    <div className="w-screen   m-auto overflow-hidden">
+        <div className='w-screen h-screen flex flex-row '>
+          <div className='bg-[rgb(226,245,252)] w-[50%] h-screen flex  justify-end '>
+            <div className=' mt-6 w-[600px]'>
+              <div className='text-[4rem] leading-none pl-10 text-[rgb(45,68,110)] font-bold'>
+                Want to be Taxsmart?
+              </div>
+              <div className=''>
+                <img src={LoginImg}/>
+              </div>
+            </div>
+          </div>
+          <div className=' w-[50%] h-screen flex  justify-start  '>
+            <div className='w-[500px] mt-6 ml-10'>
+            {formType === "signup" ? <SignupForm /> : <LoginForm />}
+            </div>
+          </div>
+        </div>
+      {/* {loading ? (
         <div className="spinner"></div>
       ) : (
         <div className='min-h-[calc(100vh-3.5rem)] m-auto w-11/12 max-w-[1260px] '> 
+          
           <div className="mx-auto flex  md:flex-row flex-col justify-between  ">
+            <div className= 'w-full md:w-[50%]'>
+              {formType === "signup" ? <SignupForm /> : <LoginForm />}
+            </div>
             <div className=" w-full md:w-[50%] ">
               <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5">
                 {heading}
@@ -39,12 +60,9 @@ function Template({img1, img2,heading,subheading1,subheading2,formType}) {
                 />
               </div>
             </div>
-            <div className= 'w-full md:w-[50%]'>
-              {formType === "signup" ? <SignupForm /> : <LoginForm />}
-            </div>
           </div>
         </div>
-      )}
+      )} */}
     </div>
     </>
   )
