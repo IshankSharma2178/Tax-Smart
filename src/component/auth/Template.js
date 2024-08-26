7f36f609dcaf94a9239155c7a38243833d796f6e
IshankSharma2178 <ishank.sharma_cs23@gla.ac.in>
2024-08-26 15:09:17 +0530

    Some Changes in UI

--- src/component/auth/Template.js
+++ src/component/auth/Template.js
@@ -1,74 +1,58 @@
 import React, { useState } from 'react'
 import SignupForm from "./SignupForm"
+import StaticNavbar from "../common/StaticNavbar"
 import LoginForm from './LoginForm'
-import StaticNavbar from '../common/StaticNavbar'
-import SignupImg from "../../assets/img/LoginImg.png"
-import LoginImg from "../../assets/img/Login.png"
 
 function Template({img1, img2,heading,subheading1,subheading2,formType}) {
 
-  const [loading , setLoading] = useState(false)
+  const [loading,setLoading] = useState(false)
 
   return (
-    <>
-    <div className='max-w-[1360px] w-[95%] m-auto relative z-[11] overflow-hidden'>
-        <StaticNavbar margin={"8"} height={"10vh"} />
-      </div>
-    <div className="w-screen   m-auto overflow-hidden">
-        <div className='w-screen h-screen flex flex-row '>
-          <div className='bg-[rgb(226,245,252)] w-[50%] h-screen flex  justify-end '>
-            <div className=' mt-6 w-[600px]'>
-              {/* <div className='text-[3rem] w-full leading-none pl-10 text-[rgb(45,68,110)] font-bold'>
-                Want to be Taxsmart?
-              </div> */}
-              <div className='my-auto'>
-                {
-                  formType === "signup"?
-                  (<img src={SignupImg}/>): (<img src={LoginImg}/>)
-                }
-              </div>
-            </div>
-          </div>
-          <div className=' w-[50%] h-screen flex  justify-start  '>
-            <div className='w-[500px] mt-6 ml-10'>
-            {formType === "signup" ? <SignupForm /> : <LoginForm />}
-            </div>
-          </div>
-        </div>
-      {/* {loading ? (
+    
+    <div className="w-11/12 max-w-[1260px] mx-auto">
+      <StaticNavbar />
+      {loading ? (
         <div className="spinner"></div>
       ) : (
-        <div className='min-h-[calc(100vh-3.5rem)] m-auto w-11/12 max-w-[1260px] '> 
-          
-          <div className="mx-auto flex  md:flex-row flex-col justify-between  ">
-            <div className= 'w-full md:w-[50%]'>
-              {formType === "signup" ? <SignupForm /> : <LoginForm />}
-            </div>
-            <div className=" w-full md:w-[50%] ">
-              <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5">
-                {heading}
-              </h1>
-              <p className="mt-4 text-[1.125rem] leading-[1.625rem]">
-                <span className="text-richblack-100">{subheading1}</span>{" "}
-                <span className="font-edu-sa font-bold italic text-blue-100">
-                  {subheading2}
-                </span>
-              </p>
-              <div className="relative md:block hidden place-content-center mx-auto w-11/12 max-w-[500px] md:mx-0">        
-                <img  
-                  alt='logo-image'
-                  width={800}
-                  loading="lazy"
-                  height={504}
-                  src={img1}
-                />
-              </div>
-            </div>
+        <div className="mx-auto flex   flex-col justify-between gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12">
+          <div className="mx-auto w-full md:w-11/12 md:max-w-[450px] md:mx-0">
+            <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5">
+              {heading}
+            </h1>
+            <p className="mt-4 text-[1.125rem] leading-[1.625rem]">
+              <span className="text-richblack-100">{subheading1}</span>{" "}
+              <span className="font-edu-sa font-bold italic text-blue-100">
+                {subheading2}
+              </span>
+            </p>
+            {formType === "signup" ? <SignupForm /> : <LoginForm />}
+          </div>
+          <div className="relative  max-w-[600px] md:mx-0">
+            {/* <img
+              src={img2}
+              alt="Pattern"
+              width={558}
+              height={504}
+              loading="lazy"
+            />
+            <img
+              src={img1}
+              alt="Students"
+              width={558}
+              height={504}
+              loading="lazy"
+              className="absolute -top-4 right-4 z-10"
+            /> */}
+
+            <img  
+              alt='logo-image'
+              loading="lazy"
+              src={img1}
+            />
           </div>
         </div>
-      )} */}
+      )}
     </div>
-    </>
   )
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Template from "../component/auth/Template"
-import images from "../assets/img/authImages/graphic5.svg"
+import Image from "../assets/img/illustration/11.png"
 
 function Signup() {
   return (
     <Template 
+      img1={Image}
       formType={"signup"}>
     </Template>
   )
